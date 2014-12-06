4b1cdd71f115158fc5741a90586d79960c564102
Petr Pilař <the.palmik@gmail.com>
2014-12-06 15:39:04 +0000

    Added a tool and data for benchmarking

---
+++ prism/bench/bench.in.pl
@@ -0,0 +1,94 @@
+{ prism => '../bin/prism'
+, models =>
+  [ { path => 'models/signalling_2_v6_noreg.sm'
+    , parameters =>
+      [ { value => 'k1=1:1.005,k2=1.995:2 5'
+        , options =>
+          [ { pse => { time => 20 } }
+          , { pse => { time => 40 } }
+          , { pse => { time => 80 } }
+          , { psecheck => { csl => 'P=? [ F=40 popR = 2 ]' } }
+          , { psecheck => { csl => 'P=? [ F=80 popR = 2 ]' } }
+          , { psecheck => { csl => 'P=? [ F=160 popR = 2 ]' } }
+          , { psecheck => { csl => 'P=? [ F=320 popR = 2 ]' } }
+          ]
+        } 
+      , { value => 'k1=1:1,k2=2:2 5'
+        , options =>
+          [ { pse => { time => 40 } }
+          , { pse => { time => 80 } }
+          , { psecheck => { csl => 'P=? [ F=160 popR = 2 ]' } }
+          , { psecheck => { csl => 'P=? [ F=320 popR = 2 ]' } }
+          ]
+        } 
+      , { value => 'k1=2:2.005,k2=3.995:4 5'
+        , options =>
+          [ { pse => { time => 20 } }
+          , { pse => { time => 40 } }
+          , { pse => { time => 80 } }
+          , { psecheck => { csl => 'P=? [ F=40 popR = 2 ]' } }
+          , { psecheck => { csl => 'P=? [ F=80 popR = 2 ]' } }
+          , { psecheck => { csl => 'P=? [ F=160 popR = 2 ]' } }
+          , { psecheck => { csl => 'P=? [ F=320 popR = 2 ]' } }
+          ]
+        }
+      , { value => 'k1=2:2,k2=4:4 5'
+        , options =>
+          [ { pse => { time => 40 } }
+          , { pse => { time => 80 } }
+          , { psecheck => { csl => 'P=? [ F=160 popR = 2 ]' } }
+          , { psecheck => { csl => 'P=? [ F=320 popR = 2 ]' } }
+          ]
+        }
+      ]
+    }
+  , { path => 'models/sir.sm'
+    , parameters =>
+      [ { value => 'ki=0.003:0.0035,kr=0.2:0.205 10'
+        , options =>
+          [ { pse => { time => 10 } }
+          , { pse => { time => 20 } }
+          , { pse => { time => 40 } }
+          , { psecheck => { csl => 'P=? [popI>0 U[100,120] popI=0]' } }
+          , { psecheck => { csl => 'P=? [popI>0 U[100,140] popI=0]' } }
+          , { psecheck => { csl => 'P=? [popI>0 U[100,180] popI=0]' } }
+          ]
+        }
+      , { value => 'ki=0.003:0.003,kr=0.2:0.2 10'
+        , options =>
+          [ { pse => { time => 10 } }
+          , { pse => { time => 20 } }
+          , { pse => { time => 40 } }
+          , { psecheck => { csl => 'P=? [popI>0 U[100,120] popI=0]' } }
+          , { psecheck => { csl => 'P=? [popI>0 U[100,140] popI=0]' } }
+          , { psecheck => { csl => 'P=? [popI>0 U[100,180] popI=0]' } }
+          ]
+        } 
+      ]
+    }
+  , { path => 'models/g1s.sm'
+    , parameters =>
+      [ { value => 'degr=0.1:0.105,prod=1:1.005 2'
+        , options =>
+          [ { pse => { time => 2 } }
+          , { pse => { time => 4 } }
+          , { pse => { time => 8 } }
+          , { pse => { time => 16 } }
+          , { psecheck => { csl => 'P=? [AA < B U[1,2] AA > B]' } }
+          , { psecheck => { csl => 'P=? [AA < B U[1,4] AA > B]' } }
+          , { psecheck => { csl => 'P=? [AA < B U[1,8] AA > B]' } }
+          , { psecheck => { csl => 'P=? [AA < B U[1,16] AA > B]' } }
+          ]
+        }
+      , { value => 'degr=0.1:0.1,prod=1:1 2'
+        , options =>
+          [ { pse => { time => 8 } }
+          , { pse => { time => 16 } }
+          , { psecheck => { csl => 'P=? [AA < B U[1,4] AA > B]' } }
+          , { psecheck => { csl => 'P=? [AA < B U[1,8] AA > B]' } }
+          ]
+        }
+      ]
+    }
+  ]
+}
---
+++ prism/bench/bench.pl
@@ -0,0 +1,93 @@
+use strict;
+use warnings;
+
+sub run_prism
+{
+  my ($prism, $args, $env_vars) = @_;
+  
+  my $env = "";
+  for my $k (keys %{$env_vars})
+  {
+    $env .= "$k=$env_vars->{$k} ";
+  }
+
+  my $command = "env $env $prism $args 2>/dev/null";
+  my $out = `$command`;
+  
+  if ($out =~ qr/Total time for model checking: ([0-9,.]*).*/)
+  {
+    return $1;
+  }
+  elsif ($out =~ qr/Time for parameter space exploration: ([0-9,.]*).*/)
+  {
+    return $1;
+  } 
+  else
+  {
+    die "Could not parse the prism output:\n$out";
+  }
+}
+
+sub run_prism_bench
+{
+  my ($prism, $args, $rep) = @_;
+ 
+  print "OCL=0 $prism $args\n";
+  my $ocl0_avg = 0;
+  for (1..$rep)
+  {
+    my $t = run_prism($prism, $args, { OCL => 0 });
+    print "$t\n";
+    $ocl0_avg += $t;
+  }
+  $ocl0_avg /= $rep;
+  print "$ocl0_avg\n"; 
+  
+  print "OCL=1 $prism $args\n";
+  my $ocl1_avg = 0;
+  for (1..$rep)
+  {
+    my $t = run_prism($prism, $args, { OCL => 1, OCL_LWS => 64 });
+    print "$t\n";
+    $ocl1_avg += $t;
+  }
+  $ocl1_avg /= $rep;
+  print "$ocl1_avg\n"; 
+}
+
+sub main
+{
+  my $def = do $ARGV[0];
+  if (my $err = $@)
+  {
+    die $@;
+  }
+  my $rep = $ARGV[1];
+
+  my @args = ();
+  for my $model (@{$def->{models}})
+  {
+    for my $parameters (@{$model->{parameters}})
+    {
+      for my $option (@{$parameters->{options}})
+      {
+        if ($option->{pse})
+        {
+          my $args = "'$model->{path}' -pse $option->{pse}{time} $parameters->{value}";
+          run_prism_bench($def->{prism}, $args, $rep);
+        }
+        elsif ($option->{psecheck})
+        {
+          my $args = "-psecheck $parameters->{value} '$model->{path}' -csl '$option->{psecheck}{csl}'";
+          run_prism_bench($def->{prism}, $args, $rep);
+        }
+        else
+        {
+          die 'Unknown option type.';
+        }
+      }
+    }
+  }
+}
+
+main();
